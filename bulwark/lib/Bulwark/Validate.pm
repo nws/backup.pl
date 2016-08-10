81b75f7648982572a823bda4df18ef5d6d2c1c48
huf <huf@keszen.hu>
2016-08-10 18:14:15 +0200

    kinda rewrote Config::Validate in pureperl with no deps but it needs heavy testing

--- bulwark/lib/Bulwark/Validate.pm
+++ bulwark/lib/Bulwark/Validate.pm
@@ -2,11 +2,21 @@ package Bulwark::Validate;
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 sub new {
 	my $class = shift;
 	my $self = bless { @_ }, $class;
 	$self->{types} = {
-
+		integer => { validate => \&_validate_integer },
+		float => { validate => \&_validate_float },
+		string => { validate => \&_validate_string },
+		boolean => { validate => \&_validate_boolean },
+		hash => { validate => \&_validate_hash },
+		array => { validate => \&_validate_array },
+		directory => { validate => \&_validate_directory },
+		file => { validate => \&_validate_file },
+		nested => { validate => sub { shift->throw("'nested' is not valid here") } },
 	};
 	return $self;
 }
@@ -19,17 +29,216 @@ sub add_type {
 
 sub validate {
 	my $self = shift;
-	my %_ = @_;
-	$self->_validate(config => $_{config}, schema => $self->{schema});
+	my %p = @_;
+	$self->_validate(config => $p{config}, schema => $self->{schema}, path => []);
+	return $p{config};
 }
 
 sub _validate {
 	my $self = shift;
-	my %_ = @_;
+	my %p = @_;
 
-	for my $key (keys %{ $_{schema} }) {
-		
+	for my $key (keys %{ $p{schema} }) {
+		my @path = (@{ $p{path} }, $key);
+		my $def = $p{schema}{ $key };
+
+
+		$self->_check_definition_type($def, \@path);
+
+		if (!exists $p{config}{ $key }) {
+			$self->throw("Required item ".$self->mkpath(\@path)." was not found") unless $def->{optional};
+			return;
+		}
+
+		if ($def->{type} eq 'nested') {
+			$self->_validate(config => $p{config}{ $key }, schema => $def->{child}, path => \@path);
+		}
+		else {
+			my $typeinfo = $self->{types}{ $def->{type} };
+			my $validate_callback = $typeinfo->{validate};
+
+			$self->throw("No validate callback defined for type '".$def->{type}."'") unless $validate_callback;
+
+			$validate_callback->($self, $p{config}{ $key }, $def, \@path);
+		}
+
+		if ($def->{callback}) {
+			$def->{callback}->($self, $p{config}{ $key }, $def, \@path);
+		}
 	}
+
+	return;
+}
+
+sub _check_definition_type {
+	my $self = shift;
+	my ($def, $path) = @_;
+	$self->throw("No type specified for ".$self->mkpath($path)) unless $def->{type};
+	$self->throw("Invalid type '".$def->{type}."' specified for ".$self->mkpath($path)) unless $self->{types}{ $def->{type} };
+}
+
+sub throw {
+	my $self = shift;
+	die @_;
+}
+
+sub _debug {}
+
+sub mkpath {
+	my $self = shift;
+	my ($path) = @_;
+
+	return join '/', @$path;
+}
+
+# default type validators
+
+sub _validate_hash {
+	my ($self, $value, $def, $path) = @_;
+
+	if (not defined $def->{keytype}) {
+		$self->throw("No keytype specified for " . $self->mkpath($path));
+	}
+
+	if (not defined $self->{types}{ $def->{keytype} }) {
+		$self->throw("Invalid keytype '$def->{keytype}' specified for " . $self->mkpath($path));
+	}
+
+	if (ref $value ne 'HASH') {
+		$self->throw(sprintf("%s: should be a 'HASH', but instead is '%s'", $self->mkpath($path), ref $value));
+	}
+
+	foreach my $k (keys %$value) {
+		my $v = $value->{$k};
+
+		my @curpath = (@$path, $k);
+		$self->_debug("Validating ", $self->mkpath(\@curpath));
+		my $callback = $self->{types}{ $def->{keytype} }{validate};
+		$callback->($self, $k, $def, \@curpath);
+		if ($def->{child}) {
+			$self->_validate(config => $v, schema => $def->{child}, path => \@curpath);
+		}
+	}
+	return;
+}
+
+sub _validate_array {
+	my ($self, $value, $def, $path) = @_;
+
+	if (not defined $def->{subtype}) {
+		$self->throw("No subtype specified for " . $self->mkpath($path));
+	}
+
+	if (not defined $self->{types}{ $def->{subtype} }) {
+		$self->throw("Invalid subtype '$def->{subtype}' specified for " . $self->mkpath($path));
+	}
+
+#	if (ref $value eq 'SCALAR' and $self->{array_allows_scalar}) {
+#		$$value = [ $$value ];
+#		$value = $$value;
+#	} elsif (ref $value eq 'REF' and ref $$value eq 'ARRAY') {
+#		$value = $$value;
+#	}
+
+	if (ref $value ne 'ARRAY') {
+		$self->throw(sprintf("%s: should be an 'ARRAY', but instead is a '%s'", $self->mkpath($path), ref $value));
+	}
+
+	my $index = 0;
+	foreach my $item (@$value) {
+		my @path = ( @$path, "[$index]" );
+		$self->_debug("Validating ", $self->mkpath(\@path));
+		my $callback = $self->{types}{ $def->{subtype} }{validate};
+		$callback->($self, $item, $def, \@path);
+		$index++;
+	}
+	return;
+}
+
+sub _validate_integer {
+	my ($self, $value, $def, $path) = @_;
+	if ($value !~ /^ -? \d+ $/xo) {
+		$self->throw(sprintf("%s should be an integer, but has value of '%s' instead", $self->mkpath($path), $value));
+	}
+	if (defined $def->{max} and $value > $def->{max}) {
+		$self->throw(sprintf("%s: %d is larger than the maximum allowed (%d)", $self->mkpath($path), $value, $def->{max}));
+	}
+	if (defined $def->{min} and $value < $def->{min}) {
+		$self->throw(sprintf("%s: %d is smaller than the minimum allowed (%d)", $self->mkpath($path), $value, $def->{max}));
+	}
+
+	return;
+}
+
+sub _validate_float {
+	my ($self, $value, $def, $path) = @_;
+	if ($value !~ /^ -? \d*\.?\d+ $/xo) {
+		$self->throw(sprintf("%s should be an float, but has value of '%s' instead", $self->mkpath($path), $value));
+	}
+	if (defined $def->{max} and $value > $def->{max}) {
+		$self->throw(sprintf("%s: %f is larger than the maximum allowed (%f)", $self->mkpath($path), $value, $def->{max}));
+	}
+	if (defined $def->{min} and $value < $def->{min}) {
+		$self->throw(sprintf("%s: %f is smaller than the minimum allowed (%f)", $self->mkpath($path), $value, $def->{max}));
+	}
+
+	return;
+}
+
+sub _validate_string {
+	my ($self, $value, $def, $path) = @_;
+
+	if (defined $def->{maxlen}) {
+		if (length($value) > $def->{maxlen}) {
+			$self->throw(sprintf("%s: length of string is %d, but must be less than %d", $self->mkpath($path), length($value), $def->{maxlen}));
+		}
+	}
+	if (defined $def->{minlen}) {
+		if (length($value) < $def->{minlen}) {
+			$self->throw(sprintf("%s: length of string is %d, but must be greater than %d", $self->mkpath($path), length($value), $def->{minlen}));
+		}
+	}
+	if (defined $def->{regex}) {
+		if ($value !~ $def->{regex}) {
+			$self->throw(sprintf("%s: regex (%s) didn't match '%s'", $self->mkpath($path), $def->{regex}, $value));
+		}
+	}
+
+	return;
+}
+
+sub _validate_boolean {
+	my ($self, $value, $def, $path) = @_;
+
+	my @true  = qw(y yes t true on);
+	my @false = qw(n no f false off);
+	$value =~ s/\s+//xg;
+	$value = 1 if any { lc($value) eq $_ } @true;
+	$value = 0 if any { lc($value) eq $_ } @false;
+
+	if ($value !~ /^ [01] $/x) {
+		$self->throw(sprintf("%s: invalid value '%s', must be: %s", $self->mkpath($path), $value, join(', ', (0, 1, @true, @false))));
+	}
+
+	return;
+}
+
+sub _validate_directory {
+	my ($self, $value, $def, $path) = @_;
+
+	if (not -d $value) {
+		$self->throw(sprintf("%s: '%s' is not a directory", $self->mkpath($path), $value));
+	}
+	return;
+}
+
+sub _validate_file {
+	my ($self, $value, $def, $path) = @_;
+
+	if (not -f $value) {
+		$self->throw(sprintf("%s: '%s' is not a file", $self->mkpath($path), $value));
+	}
+	return;
 }
 
 1;
--- bulwark/src/bulwark.pl
+++ bulwark/src/bulwark.pl
@@ -40,22 +40,23 @@ EOS
 
 exit 0;
 
-use constant IDENTIFIER_RE => qr/\A[A-Za-z_][A-Za-z0-9_-]*\z/;
-
 sub read_config {
 	my $config_file = shift;
-	if (defined(my $config = do $config_file)) {
-		return check_config_dvxsd($config);
+	my $config = do $config_file;
+
+	if (defined $config) {
+		return check_config($config);
 	}
+
 	die "$0: can not read file $config_file: $!\n" if $!;
 	die "$0: can not parse file $config_file:\n $@\n" if $@;
+	die "$0: empty config file $config_file?\n";
 }
 
-sub check_config_dvxsd {
+sub check_config {
 	my $config = shift;
-	# XXX rewrite this shit without deps (especially XS deps)
-	# AND better error handling cos this is a joke
-	use Config::Validate qw/mkpath/;
+	use Bulwark::Validate;
+
 	my $def = {
 		SETTINGS => {
 			type => 'nested',
@@ -147,102 +148,7 @@ sub check_config_dvxsd {
 	# two types of things that go into a FROM need to be distinguished and validated
 	# various strings have to be checked (value of TO, etc)
 	# stuff that goes into filenames needs more rigorous checking
-	my $validator = Config::Validate->new(schema => $def);
+	my $validator = Bulwark::Validate->new(schema => $def);
 	return $validator->validate(config => $config);
-}
-
-sub check_config {
-	my $config = shift;
-
-	die "$0: config must return a HASH ref!" unless ref $config eq 'HASH';
-
-	check_keys('toplevel', $config, [qw/SETTINGS DEFAULTS TO BACKUPS/]);
-
-	check_keys('SETTINGS', $config->{SETTINGS}, [qw/MACHINE_NAME MAILTO/]);
-	check_reftype('SETTINGS.MACHINE_NAME', $config->{SETTINGS}{MACHINE_NAME}, '');
-	check_reftype('SETTINGS.MAILTO', $config->{SETTINGS}{MAILTO}, 'ARRAY');
-
-	check_keys('DEFAULTS', $config->{DEFAULTS}, [], [qw/DB DIR GPG/]);
-
-	if ($config->{DEFAULTS}{DB}) {
-		check_keys('DEFAULTS.DB', $config->{DEFAULTS}{DB}, [], [qw/USER PASS/]);
-		check_reftype('DEFAULTS.DB.USER', $config->{DEFAULTS}{DB}{USER}, '') if $config->{DEFAULTS}{DB}{USER};
-		check_reftype('DEFAULTS.DB.PASS', $config->{DEFAULTS}{DB}{PASS}, '') if $config->{DEFAULTS}{DB}{PASS};
-	}
-
-	if ($config->{DEFAULTS}{DIR}) {
-		check_keys('DEFAULTS.DIR', $config->{DEFAULTS}{DIR}, [], [qw/EXCLUDE/]);
-		check_reftype('DEFAULTS.DIR.EXCLUDE', $config->{DEFAULTS}{DIR}{EXCLUDE}, 'ARRAY') if $config->{DEFAULTS}{DIR}{EXCLUDE};
-	}
-
-	if ($config->{DEFAULTS}{GPG}) {
-		check_keys('DEFAULTS.GPG', $config->{DEFAULTS}{GPG}, [], [qw/MODE PASS/]);
-		check_reftype('DEFAULTS.GPG.MODE', $config->{DEFAULTS}{GPG}{MODE}, '') if $config->{DEFAULTS}{GPG}{MODE};
-		check_reftype('DEFAULTS.GPG.PASS', $config->{DEFAULTS}{GPG}{PASS}, '') if $config->{DEFAULTS}{GPG}{PASS};
-	}
-
-	for my $k (keys %{ $config->{TO} }) {
-		check_re("TO.$k", $k, IDENTIFIER_RE);
-		my $dest_def = $config->{TO}{$k};
-		if ($dest_def->{DIR}) {
-			check_keys("TO.$k", $dest_def, [qw/DIR/], [qw/KEEP/]);
-			check_reftype("TO.$k.DIR", $dest_def->{DIR}, '');
-			check_reftype("TO.$k.KEEP", $dest_def->{KEEP}, '') if $dest_def->{KEEP};
-		}
-		elsif ($dest_def->{BUCKET}) {
-			check_keys("TO.$k", $dest_def, [qw/BUCKET/], [qw/KEEP CFG MODE PASS/]);
-			check_reftype("TO.$k.BUCKET", $dest_def->{BUCKET}, '');
-			for my $key (qw/KEEP CFG MODE PASS/) {
-				check_reftype("TO.$k.$key", $dest_def->{$key}, '') if $dest_def->{$key};
-			}
-		}
-		else {
-			die "$0: unknown type of TO definition $k, contains neither DIR nor BUCKET\n";
-		}
-	}
-
-	for my $k (keys %{ $config->{BUCKET} }) {
-		check_re("BACKUPS.$k", $k, IDENTIFIER_RE);
-		my $backup_def = $config->{BUCKET}{$k};
-		check_keys("BACKUPS.$k", $backup_def, [qw/FROM TO/]);
-
-		check_reftype("BACKUPS.$k.FROM", $backup_def->{FROM}, 'HASH');
-		for my $from_k (keys %{ $backup_def->{FROM} }) {
-			check_re("BACKUPS.$k.FROM.$from_k", $backup_def->{FROM}{$from_k}, IDENTIFIER_RE);
-		}
-
-
-		check_reftype("BACKUPS.$k.TO", $backup_def->{TO}, 'ARRAY');
-	}
-
-	# XXX -- more checking in general but i also forgot the check_reftype of the sections in general. they have to be hashes.
-
-	return $config;
-}
-
-sub check_keys {
-	my ($section, $hash, $required_keys, $optional_keys) = @_;
-	$optional_keys ||= [];
-
-	my %existing_keys = map +($_ => 1), keys %$hash;
-
-	for my $k (@$required_keys) {
-		die "$0: missing key in config in $section: $k\n" unless exists $hash->{$k};
-	}
-
-	delete @existing_keys{ @$required_keys, @$optional_keys };
-	die "$0: extra key(s) in config in $section: @{[ keys %existing_keys ]}\n" if keys %existing_keys;
-}
-
-sub check_reftype {
-	my ($section, $value, $want_ref_type) = @_;
-	my $got_ref_type = ref $value;
-	my $want_ref_type_label = $want_ref_type || 'SCALAR';
-	die "$0: bad value in config in $section, expected $want_ref_type_label, got @{[$got_ref_type || $value]}\n" if $got_ref_type ne $want_ref_type;
-}
-
-sub check_re {
-	my ($section, $value, $re) = @_;
-	die "$0: bad value in config in $section, expected $re, got $value\n" unless $value =~ $re;
 }
 
